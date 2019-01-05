5255f8eb4c2ef6ac9542a6b4dc1bb5ad25b078e7
arzenni <arlz.arial@gmail.com>
2019-01-05 23:40:54 +0700

    input data ke modal

--- application/views/pasien/index.php
+++ application/views/pasien/index.php
@@ -59,8 +59,6 @@
                 </button>
             </div>
             <div class="modal-body">
-            detil <?= $judul;?>
-
                 <form>
                     <div class="form-row">
                         <div class="form-group col-md-6">
@@ -69,7 +67,7 @@
                         </div>
                         <div class="form-group col-md-6">
                             <label for="norm">No. Rekam Medis</label>
-                            <input type="number" class="form-control" id="norm" placeholder="Nomor Rekam Medis">
+                            <input type="number" class="form-control" id="norm"">
                         </div>
                     </div>
                     <div class="form-group">
@@ -114,33 +112,11 @@
                     </div>
                     <button type="submit" class="btn btn-primary">Sign in</button>
                 </form>
-                
-
             </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary">Save changes</button>
         </div>
-        </div>
-      </div>
-    </div>
-        <!-- Modal -->
-    <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-           edit
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
         </div>
       </div>
     </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,24 @@ $(function(){
 
     $('.modaltampil').on('click', function(){
 
-        console.log('oukay');
-        $('#ModalLabel').html('Data Pasien');
+        // console.log('oukay');
+        $('#ModalLabel').html('Detil Pasien');
         $('.modal-footer button[type=submit]').html('Tutup');
     
         const id = $(this).data('id');
-        console.log(id);
+        // console.log(id);
 
         $.ajax({
 
             url: 'pasien/detilpasien',
             data: {id: id},
             method: 'post',
-            
+            dataType: 'json',
             success: function(data){
-                console.log(data);
+                // console.log(data);
+                $('#nik').val(data.nik);
+                $('#nama').val(data.nama);
+                $('#rt').val(data.rt);
             }
 
         });
